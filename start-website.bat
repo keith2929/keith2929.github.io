54ced83438ee67f3dcf8a487df5312760b82bbbb
keith2929 <keithtankaijun@gmail.com>
2026-03-20 15:05:10 +0800

    .

--- start-website.bat
+++ start-website.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d %~dp0
+echo Starting dev server...
+start /b npm run dev
+echo Waiting for server to start...
+timeout /t 4 /nobreak >nul
 start http://localhost:5173
-npm run dev
+echo Server is running. Press Ctrl+C in this window to stop.
 pause
